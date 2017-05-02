a33a6ea444caf7d6898cccf7eced934dce298a3c
Nick Rupp <nickrupp95@gmail.com>
2017-05-02 15:58:30 +0100

    Display single establishment

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,9 @@ class RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 
   def new
     @restaurant = Restaurant.new
