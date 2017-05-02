8c2596b13e64ce306c995834b46f3c9f380e4f8a
Naz <nazmalik08@gmail.com>
2017-05-02 15:12:29 +0100

    Add restaurants#new view and form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,18 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.all 
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
   end
 end
